26811abd12276eba4c44689999a58f0b24dced29
callanbr <chrisallan1992@icloud.com>
2020-10-27 07:06:56 -0700

    added keyEvents to change color

--- src/Application.java
+++ src/Application.java
@@ -2,8 +2,10 @@ import javax.swing.JFrame;
 
 import java.awt.image.BufferedImage;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.event.*;
 import java.awt.Color;
+import java.awt.geom.AffineTransform;
 
 public class Application extends JFrame implements MouseListener, KeyListener, MouseWheelListener {
     private final int SCREEN_WIDTH = 1280;
@@ -18,6 +20,10 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
     private int xMouseEnd = 0;
     private int yMouseStart = 0;
     private int yMouseEnd = 0;
+    private int rotateAmount = 0;
+    private int rColor = 2;
+    private int gColor = 3;
+    private int bColor = 5;
 
     public static void main(String[] args) throws Exception {
         new Application();
@@ -36,6 +42,8 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
 
     private void compute() {
         image = new BufferedImage(SCREEN_WIDTH, SCREEN_HEIGHT, BufferedImage.TYPE_INT_RGB);
+        // System.out.println(image.getGraphics());
+
         int modHeight = SCREEN_HEIGHT >> 1;
         int modWidth = SCREEN_WIDTH >> 1;
         for (int y = 0; y < SCREEN_HEIGHT; y++) {
@@ -54,17 +62,17 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
                     // Do nothing
                 } else {
 
-                    double r = iteration | (iteration << 2); // 2
+                    double r = iteration | (iteration << rColor); // 2
                     while (r > 255) {
                         r -= 255;
                     }
 
-                    double g = iteration | (iteration << 3); // 3
+                    double g = iteration | (iteration << gColor); // 3
                     while (g > 255) {
                         g -= 255;
                     }
 
-                    double b = iteration | (iteration << 5); // 5
+                    double b = iteration | (iteration << bColor); // 5
                     while (b > 255) {
                         b -= 255;
                     }
@@ -79,6 +87,9 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
     }
 
     public void paint(Graphics g) {
+        // Graphics2D g2d = (Graphics2D) g;
+        // g2d.translate(500, 0);
+        // g2d.rotate(Math.toRadians(rotateAmount));
         g.drawImage(image, 0, 0, this);
     }
 
@@ -105,7 +116,7 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
             xMouseStart = e.getX();
             yMouseStart = e.getY();
         }
-        System.out.println(10 * .008);
+        // System.out.println(10 * .008);
     }
 
     @Override
@@ -124,11 +135,6 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
     // KeyListener
     @Override
     public void keyPressed(KeyEvent e) {
-
-    }
-
-    @Override
-    public void keyReleased(KeyEvent e) {
         if (e.getKeyCode() == KeyEvent.VK_ADD || e.getKeyCode() == KeyEvent.VK_EQUALS) {
             zoom *= 0.9;
             compute();
@@ -137,12 +143,60 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
             zoom *= 1.1;
             compute();
         }
-        if (e.getKeyCode() == KeyEvent.VK_CONTROL) {
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+        if (e.getKeyCode() == KeyEvent.VK_R && e.isControlDown()) {
+            rColor += 1;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_G && e.isControlDown()) {
+            gColor += 1;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_B && e.isControlDown()) {
+            bColor += 1;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_R && e.isShiftDown()) {
+            rColor -= 1;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_G && e.isShiftDown()) {
+            gColor -= 1;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_B && e.isShiftDown()) {
+            bColor -= 1;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_PERIOD) {
             maxIterations += 5;
             compute();
         }
-        if (e.getKeyCode() == KeyEvent.VK_SHIFT) {
+        if (e.getKeyCode() == KeyEvent.VK_COMMA) {
             maxIterations -= 5;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_LEFT) {
+            xPos += 10 * zoom;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            xPos -= 10 * zoom;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_UP) {
+            yPos += 10 * zoom;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+            yPos -= 10 * zoom;
+            compute();
+        }
+        if (e.getKeyCode() == KeyEvent.VK_T) {
+            rotateAmount += 5;
             compute();
         }
     }
@@ -157,10 +211,11 @@ public class Application extends JFrame implements MouseListener, KeyListener, M
     public void mouseWheelMoved(MouseWheelEvent e) {
         int notches = e.getWheelRotation();
         if (notches < 0) {
-            System.out.println("Mouse wheel moved up " + -notches + " notches!");
-        } else {
-            System.out.println("Mouse wheel moved down " + notches + " notches!");
+            zoom *= 0.99;
         }
+        if (notches > 0) {
+            zoom *= 1.01;
+        }
+        compute();
     }
-
 }
